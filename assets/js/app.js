326425daa1908acc690698a7b97652f251e83db2
Nicolas <nicodupriez@hotmail.com>
2020-11-16 14:33:39 +0100

    install & enable sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
+
+require('../css/app.scss');
+var $ = require('jquery');
+require('bootstrap');

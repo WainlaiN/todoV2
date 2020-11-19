7a62cf856606152ef9e1dffd7b9436fdab10dee3
Nicolas <nicodupriez@hotmail.com>
2020-11-19 17:49:30 +0100

    Working on user form with role selection

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,26 @@ class UserType extends AbstractType
             ->add('email')
             //->add('roles')
             ->add('password')
+            ->add('roles', ChoiceType::class, array(
+                    'attr'  =>  array('class' => 'form-control',
+                        'style' => 'margin:5px 0;'),
+                    'choices' =>
+                        array
+                        (
+                            'ROLE_ADMIN' => array
+                            (
+                                'Yes' => 'ROLE_ADMIN',
+                            ),
+                            'ROLE_USER' => array
+                            (
+                                'Yes' => 'ROLE_USER'
+                            )
+                        )
+                ,
+                    'multiple' => false,
+                    'required' => true,
+                )
+            )
         ;
     }
 

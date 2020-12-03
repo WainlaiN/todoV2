5175a7445a90719c86e2fce00475f98dd0ff08bf
Nicolas <nicodupriez@hotmail.com>
2020-12-03 16:52:11 +0100

    working modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,22 +1,10 @@
 $(document).ready(function(e) {
-    // Initializing our modal.
-    $('#myModal').modal({
-        backdrop: 'static',
-        keyboard: false,
-        show: false,
-    });
 
     $(document).on("click", ".modalButton", function() {
 
         var id = $(this).attr('data-id')
-        var url = 'task/' + id;
+        $('#myModal' + id).modal('show');
 
-
-                $('#myModal').modal('show');
-
-
-        $(".modal-body").html("<p>" + ClickedButton + "</p> <p>Some text in the modal.</p> ");
-        $('#myModal').modal('show');
     });
 
 });

28b796fc1ccd13e81cbc48ef6f393d68ae43a00f
Nicolas <nicodupriez@hotmail.com>
2020-12-03 21:10:59 +0100

    working on using ajax for tasks general buttons

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('assign', './assets/js/assign.js')
     .addEntry('modal', './assets/js/modal.js')
+    .addEntry('button', './assets/js/button.js')
 
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')

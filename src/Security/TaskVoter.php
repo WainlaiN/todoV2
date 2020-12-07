590f5059a27a1b5671441d326bd4e3e952ad9d1b
Nicolas <nicodupriez@hotmail.com>
2020-12-07 16:18:22 +0100

    working on voter integration

---
+++ src/Security/TaskVoter.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Security;
+
+
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class TaskVoter extends Voter
+{
+    const DELETE = 'delete';
+
+    protected function supports(string $attribute, $subject)
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, [self::DELETE])) {
+            return false;
+        }
+
+        // only vote on `Task` objects
+        if (!$subject instanceof Task) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        // you know $subject is a Post object, thanks to `supports()`
+        /** @var Task $task */
+        $task = $subject;
+
+        switch ($attribute) {
+            case self::DELETE:
+                return $this->canDelete($task, $user);
+
+        }
+
+        throw new \LogicException('Vous n\'avez pas accès à cette fonction');
+
+
+    }
+}

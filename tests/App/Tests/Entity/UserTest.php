3b7714cf59a8e5a5a33202d95015bebc344f87e8
Nicolas <nicodupriez@hotmail.com>
2020-12-08 16:48:41 +0100

    add assert notblank to task

--- tests/App/Tests/Entity/UserTest.php
+++ tests/App/Tests/Entity/UserTest.php
@@ -10,10 +10,31 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class UserTest extends KernelTestCase
 {
 
-    public function testValidEntity () {
-
-        $user = (New User())
+    public function getEntity(): User
+    {
+        return (new User())
             ->setEmail("test@test.com")
+            ->setRoles(['ROLE_USER'])
+            ->setPassword('password');
+    }
+
+    public function assertHasErrors(User $user, int $number = 0)
+    {
+        self::bootKernel();
+        $error = self::$container->get('validator')->validate($user);
+        $this->assertCount($number, $error);
+
+    }
+
+    public function testValidUser()
+    {
+        $this->assertHasErrors($this->getEntity(), 0);
+
+    }
+
+    public function testInvalidUser()
+    {
+        $this->assertHasErrors($this->getEntity()->setEmail("testtest.com"), 1);
     }
 
 

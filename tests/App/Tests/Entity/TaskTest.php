3665e6459b2da0bd84bee59fa591e97ba5a37f07
Nicolas <nicodupriez@hotmail.com>
2020-12-08 17:12:14 +0100

    Added TaskTest

--- tests/App/Tests/Entity/TaskTest.php
+++ tests/App/Tests/Entity/TaskTest.php
@@ -4,7 +4,37 @@
 namespace App\Tests\Entity;
 
 
-class TaskTest
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TaskTest extends KernelTestCase
 {
+    public function getEntity(): Task
+    {
+        return (new Task())
+            ->setTitle("titre")
+            ->setContent("contenu");
+
+    }
+
+    public function assertHasErrors(Task $task, int $number = 0)
+    {
+        self::bootKernel();
+        $error = self::$container->get('validator')->validate($task);
+        $this->assertCount($number, $error);
+
+    }
+
+    public function testValidTask()
+    {
+        $this->assertHasErrors($this->getEntity(), 0);
+
+    }
+
+    public function testInvalidTask()
+    {
+        $task = $this->getEntity()->setTitle("")->setContent("");
+        $this->assertHasErrors($task, 2);
+    }
 
 }

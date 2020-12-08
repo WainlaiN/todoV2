52336330cb36f0e09db703902b0ec91c0de3dcaf
Nicolas <nicodupriez@hotmail.com>
2020-12-08 16:23:01 +0100

    testing userRepository

---
+++ tests/App/Tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Repository;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Repository\UserRepository;
+
+
+class UserRepositoryTest extends KernelTestCase
+{
+    public function testCount() {
+        self:self::bootKernel();
+        $users = self::$container->get(UserRepository::class)->count([]);
+        $this->assertEquals(23, $users);
+    }
+
+}

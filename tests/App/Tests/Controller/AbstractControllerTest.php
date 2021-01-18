2a82d12c4f0a687bbd5410dc42b57d94ee1e042e
WainlaiN <nicodupriez@hotmail.com>
2021-01-18 23:44:40 +0100

    Load fixtures in gitlab ci

--- tests/App/Tests/Controller/AbstractControllerTest.php
+++ tests/App/Tests/Controller/AbstractControllerTest.php
@@ -18,16 +18,12 @@ abstract class AbstractControllerTest extends WebTestCase
     public function loginWithAdmin(): void
     {
         $crawler = $this->client->request('GET', '/login');
-        //$form = $crawler->selectButton('Se connecter')->form();
+        $form = $crawler->selectButton('Se connecter')->form();
 
-        //$form['email']->setValue('admin@gmail.com');
-        //$form['password']->setValue('admin');
+        $form['email']->setValue('admin@gmail.com');
+        $form['password']->setValue('admin');
 
-        //$this->client->submit($form);
-
-        $this->client->submitForm('Se connecter', [
-            'email' => 'admin@gmail.com',
-            'password' => 'admin']);
+        $this->client->submit($form);
     }
 
     public function loginWithUser(): void

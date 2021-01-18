c7a91f73270a38a120802c3941d6b44655f81793
WainlaiN <nicodupriez@hotmail.com>
2021-01-18 16:39:35 +0100

    fix coding standard with phpcs on gitlab

--- src/Security/TaskVoter.php
+++ src/Security/TaskVoter.php
@@ -77,7 +77,8 @@ class TaskVoter extends Voter
     private function canEdit(Task $task, User $user)
     {
         return ($user === $task->getAssignedTo() || $this->security->isGranted(
-                'ROLE_ADMIN') || $user === $task->getUser()
+                'ROLE_ADMIN'
+            ) || $user === $task->getUser()
         );
     }
 }

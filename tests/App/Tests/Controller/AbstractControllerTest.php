acf724c7a58f6f075074758a47e1435d7d76d20a
WainlaiN <nicodupriez@hotmail.com>
2021-01-18 23:18:16 +0100

    Load fixtures in gitlab ci

--- tests/App/Tests/Controller/AbstractControllerTest.php
+++ tests/App/Tests/Controller/AbstractControllerTest.php
@@ -13,6 +13,7 @@ abstract class AbstractControllerTest extends WebTestCase
     protected function setUp(): void
     {
         $this->client = static::createClient();
+        self::bootKernel();
     }
 
     public function loginWithAdmin(): void
